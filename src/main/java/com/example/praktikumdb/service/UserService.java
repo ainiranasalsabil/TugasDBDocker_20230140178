b1b3c17d74d4d277a892f7e7917ae1c2b8408b38
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2026-04-21 11:52:37 +0700

    Service untuk mengelola data user

---
+++ src/main/java/com/example/praktikumdb/service/UserService.java
@@ -0,0 +1,17 @@
+package com.example.praktikumdb.service;
+
+import com.example.praktikumdb.model.User;
+import com.example.praktikumdb.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+// Service untuk mengelola data user
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+
+}
