4f9848949636cdd2cc9c660648c15855749e491d
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2026-04-21 11:43:51 +0700

    add comment for user entity

---
+++ src/main/java/com/example/praktikumdb/model/User.java
@@ -0,0 +1,17 @@
+package com.example.praktikumdb.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "20230140178_users")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
