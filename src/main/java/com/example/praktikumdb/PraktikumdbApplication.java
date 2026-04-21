f739bf56a4174f263cda0b7dbdcbb20014e267ad
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2026-04-21 11:38:38 +0700

    Initial commit

---
+++ src/main/java/com/example/praktikumdb/PraktikumdbApplication.java
@@ -0,0 +1,13 @@
+package com.example.praktikumdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraktikumdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraktikumdbApplication.class, args);
+	}
+
+}
