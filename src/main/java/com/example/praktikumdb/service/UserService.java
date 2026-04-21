a0e57da1a9bb66c47f66c66f6667553b3eec1311
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2026-04-21 11:53:55 +0700

    Menambahkan user baru

--- src/main/java/com/example/praktikumdb/service/UserService.java
+++ src/main/java/com/example/praktikumdb/service/UserService.java
@@ -13,10 +13,13 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
     }
 
+    public User addUser(User request){
+        request.setId(UUID.randomUUID().toString());
+        return userRepository.save(request);
+    }
 
 }
